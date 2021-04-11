1b781118556f8f4896903cbc9fc962e4ef066b42
Makkenzie Zepeda <Makkenzie1@ymail.com>
2021-04-10 18:22:04 -0600

    Ships can be destroyed

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -50,12 +50,12 @@ class Play extends Phaser.Scene {
 
          // green UI background
          this.add.rectangle(
-             0, 
-             borderUISize + borderPadding, 
-             game.config.width, 
-             borderUISize*2,
-             0x00FF00,
-             ).setOrigin(0,0);
+            0, 
+            borderUISize + borderPadding, 
+            game.config.width, 
+            borderUISize*2,
+            0x00FF00,
+            ).setOrigin(0,0);
 
 
         // white borders
@@ -70,14 +70,29 @@ class Play extends Phaser.Scene {
         keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);  
      }
 
-     update(){
-         this.starfield.tilePositionX -= 4;
-         this.p1Rocket.update();
+    update(){
+        this.starfield.tilePositionX -= 4;
+        this.p1Rocket.update();
          
-         this.ship1.update();
-         this.ship2.update();
-         this.ship3.update();
-         
+        this.ship1.update();
+        this.ship2.update();
+        this.ship3.update();
 
-     }
+        this.checkCollision(this.p1Rocket, this.ship1);
+        this.checkCollision(this.p1Rocket, this.ship2);
+        this.checkCollision(this.p1Rocket, this.ship3);
+
+    }
+
+    checkCollision(rocket, ship) {
+        if( rocket.x + rocket.width > ship.x &&
+            rocket.x < ship.x + ship.width &&
+            rocket.y + rocket.height > ship.y &&
+            rocket.y < ship.y + ship.height) {
+                ship.alpha = 0;
+                rocket.reset();
+                ship.reset();
+            }
+
+    }
 }
