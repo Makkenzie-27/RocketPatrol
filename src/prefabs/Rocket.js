e3e2d094ca1f148e4ac33979d9e23241a1d9e206
Makkenzie Zepeda <Makkenzie1@ymail.com>
2021-04-11 12:05:31 -0600

    GOT SPACESHIP TO WORK

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -47,4 +47,10 @@ class Rocket extends Phaser.GameObjects.Sprite{
         }
         
     }
+
+    //reset rocket to "ground"
+    reset() {
+        this.isFiring = false;
+        this.y = game.config.height - borderUISize -borderPadding;
+    }
 }
--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -48,12 +48,26 @@ class Play extends Phaser.Scene {
        
 
     update() {
+
+
         this.starfield.tilePositionX -= 4;
 
         this.p1Rocket.update();
+        
         this.ship01.update();               // update spaceships (x3)
         this.ship02.update();
         this.ship03.update();
+
+        // check collisions
+        if(this.checkCollision(this.p1Rocket, this.ship03)) {
+           console.log('kaboom ship 03');
+        }
+        if (this.checkCollision(this.p1Rocket, this.ship02)) {
+            console.log('kaboom ship 02');
+        }
+        if (this.checkCollision(this.p1Rocket, this.ship01)) {
+            console.log('kaboom ship 01');
+        }
         
     
     }
